9d2d7ce4e3a6c375d828c8de395a05417f4a9fb8
alekskhryplyvets <106542516+AleksKhryplyvets@users.noreply.github.com>
2023-03-12 21:46:52 +0200

    Update Button.jsx

--- my/src/Button.jsx
+++ my/src/Button.jsx
@@ -1,5 +1,6 @@
 const Button = (props) => {
-  return <button> {props.text}</button>;
+  const {text, onClick} = props
+  return <button onClick={onClick}> {text}</button>;
 };
 
 export default Button;
