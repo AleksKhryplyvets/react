9abae68901abbc1a32aa5574bb800b19397ca83b
alekskhryplyvets <106542516+AleksKhryplyvets@users.noreply.github.com>
2023-03-12 21:46:31 +0200

    Update App.jsx

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import Button from "./Button";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
-      <h1>Hello React</h1>
-      <Button text="Click me" />
+      <div>{count}</div>
+      <div>
+        <Button text="Click me for plus" onClick={incrementCount} />
+      </div>
+      <div>
+        <Button text="Click me for minus" onClick={decrementCount} />
+      </div>
     </div>
   );
 }
