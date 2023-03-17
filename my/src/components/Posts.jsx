d09451f48635bb493b2b69449fe7b2257034979d
alekskhryplyvets <106542516+AleksKhryplyvets@users.noreply.github.com>
2023-03-17 20:45:41 +0200

    Update Posts.jsx

--- my/src/components/Posts.jsx
+++ my/src/components/Posts.jsx
@@ -1,1 +1,28 @@
+import { useEffect, useState } from "react";
 
+const Posts = () => {
+  const [value, setValue] = useState(0)
+  const [posts, setPosts] = useState([]);
+  const handleClick = () => setPosts(value + 1)
+
+  useEffect(() => {
+    const request = async () => {
+      const responce = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const items = await responce.json();
+      setPosts(items);
+    };
+    request();
+  }, []);
+  return (
+    <div className="posts">
+      <ul>
+        {posts.map(post => <li key={post.id}>{post.name}</li>)}
+      </ul>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+};
+
+export default Posts;
